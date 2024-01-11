202b49f5882b469e775bf646e3b510ab9286ae28
Reletiv <wietsehouwers@gmail.com>
2024-01-11 22:55:08 +0100

    fix inverse colours

--- ARM_Tag_FW/OpenEPaperLink_TLSR/src/epd_bwr_213.c
+++ ARM_Tag_FW/OpenEPaperLink_TLSR/src/epd_bwr_213.c
@@ -136,14 +136,6 @@ BWR_213_Len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     return epd_temperature;
 }
 
-
-
-
-
-
-
-
-
  uint8_t EPD_BWR_213_Display(unsigned char *image, int size, uint8_t full_or_partial)
 {    
     uint8_t epd_temperature = 0 ;
@@ -264,14 +256,6 @@ BWR_213_Len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     return epd_temperature;
 }
 
-
-
-
-
-
-
-
-
  void EPD_BWR_213_set_sleep(void)
 {
     // deep sleep
@@ -279,206 +263,6 @@ BWR_213_Len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     EPD_WriteData(0x01);
 
 }
-
-
-
-
-
-// uint8_t EPD_BWR_213_Display_start(uint8_t full_or_partial)
-// {
-//     uint8_t epd_temperature = 0;
-
-//     // SW Reset
-//     EPD_WriteCmd(0x12);
-
-//     EPD_CheckStatus_inverted(100);
-
-//     EPD_WriteCmd(0x3c);
-//     EPD_WriteData(0x05);
-
-//     EPD_WriteCmd(0x18);
-//     EPD_WriteData(0x80);
-
-//     EPD_WriteCmd(0x22);
-//     EPD_WriteData(0xb1);
-
-//     EPD_WriteCmd(0x20);
-
-//     EPD_CheckStatus_inverted(100);
-
-//     EPD_WriteCmd(0x1B);
-//     epd_temperature = EPD_SPI_read();
-//     EPD_SPI_read();
-
-//     WaitMs(5);
-
-//     EPD_WriteCmd(0x21);
-//     EPD_WriteData(0b00000000);
-//     EPD_WriteData(0x00);
-
-//     EPD_WriteCmd(0x11);
-//     EPD_WriteData(0x03);
-
-//     // // Set RAM X- Address Start/End
-//     // EPD_WriteCmd(0x44);
-//     // EPD_WriteData(0x01);
-//     // EPD_WriteData(0x17);
-
-//     // // Set RAM Y- Address Start/End
-//     // EPD_WriteCmd(0x45);
-//     // EPD_WriteData(0x00);
-//     // EPD_WriteData(0x00);
-//     // EPD_WriteData(0x7f);
-//     // EPD_WriteData(0x01);
-
-
-//         // Set RAM X- Address Start/End
-//     EPD_WriteCmd(0x44);
-//     EPD_WriteData(0x00);
-//     EPD_WriteData(0x0F);
-
-//     // Set RAM Y- Address Start/End
-//     EPD_WriteCmd(0x45);
-//     EPD_WriteData(0x28);
-//     EPD_WriteData(0x01);
-//     EPD_WriteData(0x2E);
-//     EPD_WriteData(0x00);
-
-
-//     // Set RAM X address
-//     EPD_WriteCmd(0x4E);
-//     EPD_WriteData(0x00);
-
-//     // Set RAM Y address
-//     EPD_WriteCmd(0x4F);
-//     EPD_WriteData(0x28);
-//     EPD_WriteData(0x01);
-
-//     EPD_WriteCmd(0x24);
-
-//     return epd_temperature;
-// }
-
-
-
-
-
-
-//  uint8_t EPD_BWR_213_Display_start(uint8_t full_or_partial)
-// {
-//     uint8_t epd_temperature = 0;
-
-//     // SW Reset
-//     EPD_WriteCmd(0x12);
-
-//     EPD_CheckStatus_inverted(100);
-
-//     // Set Analog Block control
-//     EPD_WriteCmd(0x74);
-//     EPD_WriteData(0x54);
-//     // Set Digital Block control
-//     EPD_WriteCmd(0x7E);
-//     EPD_WriteData(0x3B);
-
-//     // ACVCOM Setting
-//     EPD_WriteCmd(0x2B);
-//     EPD_WriteData(0x04);
-//     EPD_WriteData(0x63);
-
-//     // Booster soft start
-//     EPD_WriteCmd(0x0C);
-//     EPD_WriteData(0x8B);
-//     EPD_WriteData(0x9C);
-//     EPD_WriteData(0x96);
-//     EPD_WriteData(0x0F);
-
-//     // Driver output control
-//     EPD_WriteCmd(0x01);
-//     EPD_WriteData(0x28);
-//     EPD_WriteData(0x01);
-//     EPD_WriteData(0x01);
-
-//     // Data entry mode setting
-//     EPD_WriteCmd(0x11);
-//     EPD_WriteData(0x02);
-
-//     // Temperature sensor control
-//     EPD_WriteCmd(0x18);
-//     EPD_WriteData(0x80);
-
-//     // Set RAM X- Address Start/End
-//     EPD_WriteCmd(0x44);
-//     EPD_WriteData(0x00);
-//     EPD_WriteData(0x0C);
-
-//     // Set RAM Y- Address Start/End
-//     EPD_WriteCmd(0x45);
-//     EPD_WriteData(0x28);
-//     EPD_WriteData(0x01);
-//     EPD_WriteData(0x54);
-//     EPD_WriteData(0x00);
-
-//     // Border waveform control
-//     EPD_WriteCmd(0x3C);
-//     EPD_WriteData(0x01);
-
-//     // Display update control
-//     EPD_WriteCmd(0x22);
-//     EPD_WriteData(0xA1);
-
-//     // Master Activation
-//     EPD_WriteCmd(0x20);
-
-//     EPD_CheckStatus_inverted(100);
-
-//     // Temperature sensor read from register
-//     EPD_WriteCmd(0x1B);
-//     epd_temperature = EPD_SPI_read();
-//     EPD_SPI_read();
-
-//     WaitMs(5);
-
-//     // Display update control
-//     EPD_WriteCmd(0x22);
-//     EPD_WriteData(0xB1);
-
-//     // Master Activation
-//     EPD_WriteCmd(0x20);
-
-//     EPD_CheckStatus_inverted(100);
-
-//     // Display update control
-//     EPD_WriteCmd(0x21);
-//     EPD_WriteData(0x03);
-
-//     // Set RAM X address
-//     EPD_WriteCmd(0x4E);
-//     EPD_WriteData(0x00);
-
-//     // Set RAM Y address
-//     EPD_WriteCmd(0x4F);
-//     EPD_WriteData(0x28);
-//     EPD_WriteData(0x01);
-
-//     // Display update control
-//     EPD_WriteCmd(0x22);
-//     EPD_WriteData(0x40);
-
-//     int i;
-//     if (!full_or_partial)
-//     {
-//         EPD_WriteCmd(0x32);
-//         for (i = 0; i < sizeof(LUT_bwr_213_part); i++)
-//         {
-//             EPD_WriteData(LUT_bwr_213_part[i]);
-//         }
-//     }
-
-//     EPD_WriteCmd(0x24);
-
-//     return epd_temperature;
-// }
-
 
  uint8_t EPD_BWR_213_Display_start(uint8_t full_or_partial)
 {
@@ -565,9 +349,9 @@ BWR_213_Len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 
     // Display update control
     EPD_WriteCmd(0x21);
-    EPD_WriteData(0x03);
+    EPD_WriteData(0x08);
 
-        // Set RAM X address
+    // Set RAM X address
     EPD_WriteCmd(0x4E);
     EPD_WriteData(0x00);
 
@@ -595,10 +379,6 @@ BWR_213_Len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     return epd_temperature;
 }
 
-
-
-
-
  void EPD_BWR_213_Display_byte(uint8_t data)
 {
     EPD_WriteData(data);
@@ -611,9 +391,10 @@ BWR_213_Len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         EPD_WriteData(image[i]);
     }
 }
+
  void EPD_BWR_213_Display_end()
 {
-        // Set RAM X address
+    // Set RAM X address
     EPD_WriteCmd(0x4E);
     EPD_WriteData(0x00);
 
@@ -630,6 +411,7 @@ BWR_213_Len, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     EPD_WriteCmd(0x20);
     EPD_WriteCmd(0x24);
 }
+
  void EPD_BWR_213_Display_color_change()
 {
     // // Set RAM X address
